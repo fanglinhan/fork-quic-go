31e2f101628301927bd432ec7aef11098f780598
aggresss <aggresss@163.com>
2023-08-24 09:22:13 +0800

    2023-08-24 09:22:13 CST W34D4

--- internal/congestion/bandwidth_sampler_test.go
+++ internal/congestion/bandwidth_sampler_test.go
@@ -1,1 +1,20 @@
 package congestion
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("unit", func() {
+	var ()
+
+	BeforeEach(func() {
+
+	})
+
+	It("test", func() {
+		Expect(0).To(Equal(0))
+		Expect(true).To(BeTrue())
+		Expect(func() { panic("") }).To(Panic())
+	})
+})
